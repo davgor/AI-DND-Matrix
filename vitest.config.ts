0de863edd91de5f63b045e6a5292d33141a36580
davgor <davgor4@gmail.com>
2026-06-30 01:43:07 -0500

    gross

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
-    environment: 'node'
+    environment: 'node',
+    testTimeout: 15_000
   }
 })
